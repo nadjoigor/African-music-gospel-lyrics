0252a7b16b26e4880fe0399fb820eed583b5fb92
BABATOUNDE Charlotte <charlottebabatounde46@gmail.com>
2026-09-02 14:42:56 +0200

    implémentation de la view et des urls

--- backend/music_gospel/chants/views.py
+++ backend/music_gospel/chants/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import Chant
 
 # Create your views here.
+def accueil(request):
+    chants = Chant.objects.all() 
+    
+    return render(request, 'chants/acceuil.html', {'chants': chants})
